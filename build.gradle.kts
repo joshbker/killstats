b469d4d80612f4f9671b84eb4100ecd53ca85a2d
Josh <43449531+joshbker@users.noreply.github.com>
2025-03-27 20:44:53 +0000

    fix: shade kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    id("com.gradleup.shadow") version "8.3.6"
 }
 
 group = "gg.joshbaker"
@@ -14,6 +15,16 @@ repositories {
 dependencies {
     compileOnly("io.papermc.paper:paper-api:1.20.2-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.6")
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+
+    shadowJar {
+        archiveFileName.set("killstats-${version}.jar")
+    }
 }
 
 kotlin {
